51460c4ab6a4e51e19bc3555a9c47e5b11005b1b
ImamDwiYanPermana <imampermana224@gmail.com>
2023-03-17 17:27:54 +0700

    Meng deklarasikan ukuran Array dan Variable n

--- 086_BubbleSort/086_BubbleSort.cpp
+++ 086_BubbleSort/086_BubbleSort.cpp
@@ -1,5 +1,10 @@
 
 #include <iostream>
+using namespace std;
+
+int a[20];                              // Deklarasi Array a dengan ukuran 20
+int n;                                  // Deklarasi Variable n untuk menyimpan banyak nya array pada element pada array
+
 
 int main()
 {
